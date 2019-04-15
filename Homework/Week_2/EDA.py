20be5a27ec5b7451b2d33bde2f56fb43b2616d57
Tim Verlaan <timverlaan@live.nl>
2019-04-15 11:17:33 +0200

    neater

--- Homework/Week_2/EDA.py
+++ Homework/Week_2/EDA.py
@@ -5,37 +5,49 @@ import matplotlib.pyplot as plt
 from scipy import stats
 import seaborn as sns
 
-def set_up():
+csv_file = "input.csv"
     
-    # strip the white spaces from the region column
-    with open("input.csv") as f:
-        reader = csv.reader(f, delimiter=",")
-        with open("stripped.csv", "w") as fo:
-            writer = csv.writer(fo)
-            for row in reader:
-                writer.writerow([e.strip() for e in row])
+# strip the white spaces from the region column
+with open(csv_file) as f:
+    reader = csv.reader(f, delimiter=",")
+    with open("stripped.csv", "w") as fo:
+        writer = csv.writer(fo)
+        for row in reader:
+            writer.writerow([e.strip() for e in row])
 
-    # list with stand-ins for empty cells
-    missing_values = ["n/a", "na", "unknown", "-", ""]
+stripped_csv = "stripped.csv"
 
-    # set missing values to NaN
-    df_bef = pd.read_csv("stripped.csv", na_values = missing_values, skipinitialspace = True)
+data = {}
+with open(stripped_csv) as file:
+    csv_reader = csv.DictReader(stripped_csv)
+    for row in csv_reader:
+        country = row["Country"]
+        data[country] = row
 
-    # drop columns we won't be using
-    df = df_bef.drop(columns=['Population', 'Area (sq. mi.)', 'Coastline (coast/area ratio)', 'Net migration', 
-                            'Literacy (%)', 'Phones (per 1000)', 'Arable (%)', 'Crops (%)', 'Other (%)', 
-                            'Climate', 'Birthrate', 'Deathrate', 'Agriculture', 'Industry', 'Service'])
+print(data)
 
-    # pd.set_option('display.max_columns', 5)
-    # pd.set_option("display.max_rows", 227)
-    
-    # strip the string 'dollar' and set remaining value to floats
-    df['GDP ($ per capita) dollars'] = df['GDP ($ per capita) dollars'].str.lstrip('+-').str.rstrip('dollars').astype(float)
 
-    # unify the region column
-    df.loc[df['Region'].str.contains('NEAR EAST'), 'Region'] = 'ASIA (EX. NEAR EAST)'
+# list with stand-ins for empty cells
+missing_values = ["n/a", "na", "unknown", "-", ""]
 
-    print(df)
+# set missing values to NaN
+df_bef = pd.read_csv("stripped.csv", na_values = missing_values, skipinitialspace = True)
+
+# drop columns we won't be using
+df = df_bef.drop(columns=['Population', 'Area (sq. mi.)', 'Coastline (coast/area ratio)', 'Net migration', 
+                        'Literacy (%)', 'Phones (per 1000)', 'Arable (%)', 'Crops (%)', 'Other (%)', 
+                        'Climate', 'Birthrate', 'Deathrate', 'Agriculture', 'Industry', 'Service'])
+
+# pd.set_option('display.max_columns', 5)
+# pd.set_option("display.max_rows", 227)
+
+# strip the string 'dollar' and set remaining value to floats
+df['GDP ($ per capita) dollars'] = df['GDP ($ per capita) dollars'].str.lstrip('+-').str.rstrip('dollars').astype(float)
+
+# unify the region column
+df.loc[df['Region'].str.contains('NEAR EAST'), 'Region'] = 'ASIA (EX. NEAR EAST)'
+
+print(df)
 
 def central_tendency():
 
@@ -50,9 +62,21 @@ def central_tendency():
     # df['GDP ($ per capita) dollars'].plot()
     # plt.show()
 
-if __name__ == "__main__":
+def five_num_sum ():
+    
+    pass
 
-    set_up()
+def converting():
+
+    pass
+
+
+if __name__ == "__main__":
     
     central_tendency()
+
+    # five_num_sum()
+
+    # converting()
+
 

d598415a5aa3865cfac4a995857c3471a7b4722c
Tim Verlaan <timverlaan@live.nl>
2019-05-07 15:59:40 +0200

    final

--- Homework/Week_4/converter.py
+++ Homework/Week_4/converter.py
@@ -1,6 +1,7 @@
 import pandas as pd
 df = pd.read_csv('dataSmall.csv')
+df = df.sort_values('Value')
 df = df[['LOCATION', 'TIME', 'Value']].to_json(orient='records')
 
-f = open( 'data_oecd_small.json', 'w')  
+f = open( 'data_oecd_small_sort.json', 'w')  
 f.write(df) 

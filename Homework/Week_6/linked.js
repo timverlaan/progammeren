28b6b2a9463c9d855f0be20c0e4a45f244b0c4ec
Tim Verlaan <timverlaan@live.nl>
2019-05-21 16:05:54 +0200

    tuesday upload

--- Homework/Week_6/linked.js
+++ Homework/Week_6/linked.js
@@ -8,7 +8,7 @@ window.onload = function(){
 
 function loadData(){
 
-    var datafile = "result2.json";
+    var datafile = "result1.json";
 
     d3.json(datafile).then(function(data) {
         console.log(data);
@@ -19,7 +19,7 @@ function loadData(){
 
 function drawBar(data){
     // create the SVG (dimensions in sinc with the x and y axis)
-    var svg = d3.select("body").append("svg").attr("width", "960").attr("height", "500");     
+    var svg = d3.select("body").append("svg").attr("width", "1260").attr("height", "500");     
     
     // set margins for inner g element 
     var margin = 
@@ -35,51 +35,52 @@ function drawBar(data){
     var barPadding = 1
 
     var xScale = d3.scaleBand()     // make seperate bins for every country
-        // .domain(data.map(function (d) { return d.country; }))
-        .domain([0, 50])
+        .domain(data.map(function (d) { return d.country; }))
+        // .domain([0, 50])
         .range([0, w]);
 
     var yScale = d3.scaleLinear()
-        .domain([0, 20])             // data is normalized
+        .domain([0, 30])             // data is normalized
         .range([h, 0]);
     
     var ColourScale = d3.scaleLinear()
-        .domain([0.25, 0.6])        // make the bar colour go from blue to black
+        .domain([0, 27])        // make the bar colour go from blue to black
         .range([0, 255])
 
     var tip = d3.tip()              // displays data when hovering
         .attr('class', 'd3-tip')
         .offset([-20, 0])
         .html(function(d) {
-            return "<strong>GINI Value:</strong> <span style='color:red'>" + d.country + "</span>";
+            return "<strong>Country:</strong> <span style='color:red'>" + d.country + "</span>";
         })    
 
         svg.call(tip);        
-
-    console.log(data[Object.keys(data)[1]])
 
     g.selectAll(".bar")
         .data(data)
         .enter()
         .append("rect")
         .attr("class", "bar")       // make hover
-        .attr("width", (w / data.length - barPadding))
+        .attr("width", (w / 42 - barPadding))
         .attr("x", function(d)
         {             
             return xScale(d.country);
         })     
-        // .attr("fill", function(d) {
-        //     return "rgb(0, 0, " + ColourScale(d.Value) + ")"; // minus smallest value to have get a nice spectrum
-        // })
+        .attr("fill", function(d) {
+            return "rgb(0, 0, " + ColourScale(d.lengthList) + ")"; // minus smallest value to have get a nice spectrum
+        })
         .attr("y", function(d) {
-            return yScale(d.country.length); //Height minus data value
+            return yScale(d.lengthList); //Height minus data value
         })      
         .attr("height", function(d)
         {
-            return  h - yScale(d.country.length);
+            return  h - yScale(d.lengthList);
         })         
         .on('mouseover', tip.show)
         .on('mouseout', tip.hide)       // make dynamic
+        .on('click', function(d){
+            drawPie(d)
+        })
 
     // Add x-axis
     g.append("g")
@@ -120,5 +121,71 @@ function drawBar(data){
                     (-20) + ")")
         .style("text-anchor", "middle")
         .text("Journalists Killed in Foreign Countries");
+
+        drawPie(data);
+
+}
+
+function drawPie(data){
+
+    let dataset = Array.isArray(data) ? data[0] : data
+    console.log(dataset);
+    // set the dimensions and margins of the graph
+    var widthPC = 450
+        heightPC = 450
+        marginPC = 10
+
+    // The radius of the pieplot is half the width or half the height (smallest one). I substract a bit of margin.
+    var radius = Math.min(widthPC, heightPC) / 2 - marginPC
+
+    // append the svg object to the div called 'my_dataviz'
+    var svg = d3.select("body")
+        .append("svg")
+            .attr("width", widthPC)
+            .attr("height", heightPC)
+        .append("g")
+        .attr("transform", "translate(" + widthPC / 2 + "," + heightPC / 2 + ")");
+
+    // set the color scale
+    var color = d3.scaleOrdinal()
+        .domain(data)
+        .range(d3.schemeSet2)
+
+    // Compute the position of each group on the pie:
+    var pie = d3.pie()
+        .value(function(d) { return color(d.data); })
+    var data_ready = pie(d3.entries(data['pieChart']))
+    // Now I know that group A goes from 0 degrees to x degrees and so on.    
+
+    // shape helper to build arcs:
+    var arcGenerator = d3.arc()
+        .innerRadius(0)
+        .outerRadius(radius)
+
+    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+    svg
+    .selectAll('mySlices')
+    .data(data_ready)
+    .enter()
+    .append('path')
+        .attr('d', arcGenerator)
+        // .attr('fill', function(d){ return (d.data.value.value) })
+        .attr('fill', 'blue')
+        .attr("stroke", "black")
+        .style("stroke-width", "2px")
+        .style("opacity", 0.7)
+
+    // Now add the annotation. Use the centroid method to get the best coordinates
+    svg
+    .selectAll('mySlices')
+    .data(data_ready)
+    .enter()
+    .append('text')
+        .text(function(d){ 
+            console.log(d.data.value) 
+            return "grp " + d.data.value.gender})
+        .attr("transform", function(d) { return "translate(" + arcGenerator.centroid(d) + ")";  })
+        .style("text-anchor", "middle")
+        .style("font-size", 17)
 
 }
